cbefe140e856628c4b1f72c75941ab60c4dd0ae0
Robert Samoraj <rsamoraj11@gmail.com>
2026-06-02 20:17:42 -0500

    Add test file script

---
+++ echor/tests/cli.rs
@@ -0,0 +1,31 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+use std::fs;
+
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
+#[test]
+fn dies_no_arg() -> TestResult
+{
+    let mut cmd = Command::cargo_bin("echor")?;
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("USAGE"));
+    Ok(())
+}
+
+#[test]
+fn runs()
+{
+    let mut cmd = Command::cargo_bin("echor").unwrap();
+    cmd.arg("hello").assert().success();
+}
+
+#[test]
+fn hello1() -> TestResult
+{
+    let expected = fs::read_to_string("tests/expected/hello1.txt")?;
+    let mut cmd = Command::cargo_bin("echor")?;
+    cmd.arg("Hello there").assert().success().stdout(expected);
+    Ok(())
+}
